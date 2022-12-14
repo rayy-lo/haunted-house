0c5113c51236d1c4e1bcb64c89d5380aabf45a0e
Raymond Lo <raymond.lo@yahoo.com>
2022-12-14 01:06:43 -0500

    Setup World

--- src/World/World.ts
+++ src/World/World.ts
@@ -1,18 +1,33 @@
-import { Sizes, SizesI } from "./Utils/Sizes";
+import WorldScene from "./components/Scene";
+import { Sizes } from "./Utils/Sizes";
+import { WorldCamera } from "./components/Camera";
+import Renderer from "./components/Renderer";
 
 export default class World {
-  canvas: HTMLCanvasElement;
-  sizes: SizesI;
+  private canvas: HTMLCanvasElement;
+  private camera: WorldCamera;
+  private scene: WorldScene;
+  private sizes: Sizes;
+  private renderer: Renderer;
 
-  constructor(canvas) {
+  constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
 
     // Setup
+    this.scene = new WorldScene();
     this.sizes = new Sizes();
+    this.camera = new WorldCamera(this.sizes, this.canvas, this.scene);
+    this.renderer = new Renderer(
+      this.sizes,
+      this.canvas,
+      this.scene,
+      this.camera
+    );
+
     this.sizes.on("resize", this.handleResize.bind(this));
   }
 
   handleResize() {
-    console.log("resize");
+    this.camera.resize();
   }
 }
