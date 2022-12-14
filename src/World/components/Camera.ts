fae5660daecc62406d22616657c7ebf2c10c2d94
Raymond Lo <raymond.lo@yahoo.com>
2022-12-14 01:07:12 -0500

    Setup Camera with OrbitControls

--- src/World/components/Camera.ts
+++ src/World/components/Camera.ts
@@ -1,16 +1,40 @@
-import { Camera, PerspectiveCamera } from "three";
+import { PerspectiveCamera } from "three";
+import { OrbitControls } from "../../../node_modules/three/examples/jsm/controls/OrbitControls.js";
 
-export class MyCamera extends Camera {
-  // Custom properties and methods here
-  private camera: Camera;
+export class WorldCamera {
+  private camera: PerspectiveCamera;
+  private controls: OrbitControls;
+  private sizes;
+  private canvas;
+  private scene;
 
-  constructor() {
-    super();
-    this.camera = new PerspectiveCamera();
-    // Initialize the camera here
+  constructor(sizes, canvas, scene) {
+    this.sizes = sizes;
+    this.canvas = canvas;
+    this.scene = scene;
+
+    this.setInstance();
+    this.setOrbitControls();
   }
 
-  update() {
-    // Update the camera here
+  update() {}
+
+  setInstance() {
+    this.camera = new PerspectiveCamera(
+      35,
+      this.sizes.width / this.sizes.height
+    );
+    this.camera.position.set(0, 0, 10);
+    this.scene.add(this.camera);
+  }
+
+  setOrbitControls() {
+    this.controls = new OrbitControls(this.camera, this.canvas);
+    this.controls.enableDamping = true;
+  }
+
+  resize() {
+    this.camera.aspect = this.sizes.width / this.sizes.height;
+    this.camera.updateProjectionMatrix();
   }
 }
