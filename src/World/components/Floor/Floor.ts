289b55ba332378676cf2a73c879de353f7a4f608
Raymond Lo <raymond.lo@yahoo.com>
2022-12-15 03:01:08 -0500

    Change to light reflective material

--- src/World/components/Floor/Floor.ts
+++ src/World/components/Floor/Floor.ts
@@ -1,4 +1,4 @@
-import { Mesh, MeshBasicMaterial, PlaneGeometry } from "three";
+import { Mesh, MeshStandardMaterial, PlaneGeometry } from "three";
 import World from "../../World";
 
 export default class Floor {
@@ -8,7 +8,7 @@ export default class Floor {
     this.world = World.getInstance();
 
     const planeGeometry = new PlaneGeometry(10, 10);
-    const planeMaterial = new MeshBasicMaterial({ color: "#d0e9bb" });
+    const planeMaterial = new MeshStandardMaterial({ color: "#d0e9bb" });
     const floor = new Mesh(planeGeometry, planeMaterial);
     floor.rotation.x = Math.PI * -0.5;
     floor.position.set(0, -1, 0);
--- src/World/components/House/House.ts
+++ src/World/components/House/House.ts
@@ -2,11 +2,11 @@ import {
   BoxGeometry,
   Mesh,
   MeshStandardMaterial,
-  MeshBasicMaterial,
   Group,
   ConeGeometry,
   PlaneGeometry,
   SphereGeometry,
+  PointLight,
 } from "three";
 import World from "../../World";
 
@@ -18,31 +18,46 @@ export default class House {
     this.world = World.getInstance();
     this.house = new Group();
 
+    // Walls
     const cubeGeometry = new BoxGeometry(4, 2, 4);
-    const cubeMaterial = new MeshBasicMaterial({
+    const cubeMaterial = new MeshStandardMaterial({
       color: "#ac8e82",
     });
     const cube = new Mesh(cubeGeometry, cubeMaterial);
     this.house.add(cube);
 
+    // Roof
     const coneGeometry = new ConeGeometry(3.5, 2, 4);
-    const coneMaterial = new MeshBasicMaterial({ color: "#b35f45" });
+    const coneMaterial = new MeshStandardMaterial({ color: "#b35f45" });
     const roof = new Mesh(coneGeometry, coneMaterial);
     roof.position.set(0, 2, 0);
     roof.rotation.y = Math.PI / 4;
     this.house.add(roof);
 
+    // Door
     const planeGeometry = new PlaneGeometry(2, 1.5);
-    const planeMaterial = new MeshBasicMaterial({ color: "#b35f45" });
+    const planeMaterial = new MeshStandardMaterial({ color: "#b35f45" });
     const door = new Mesh(planeGeometry, planeMaterial);
     door.position.set(0, -0.25, 2.01);
     this.house.add(door);
 
+    //Bushes
     const sphereGeometry = new SphereGeometry(0.5);
-    const sphereMaterial = new MeshBasicMaterial({ color: "#89c854" });
+    const sphereMaterial = new MeshStandardMaterial({ color: "#89c854" });
     const bush = new Mesh(sphereGeometry, sphereMaterial);
-    bush.position.set(1, -0.75, 2);
+    bush.position.set(0.75, -0.75, 2);
+
     this.house.add(bush);
+
+    const bush2 = new Mesh(sphereGeometry, sphereMaterial);
+    bush2.scale.set(0.5, 0.5, 0.5);
+    bush2.position.set(1.25, -1, 2);
+    this.house.add(bush2);
+
+    //Doorlight
+    const doorLight = new PointLight("#ff7d46", 1);
+    doorLight.position.set(0, 1, 2.5);
+    this.house.add(doorLight);
 
     this.world.scene.add(this.house);
   }
