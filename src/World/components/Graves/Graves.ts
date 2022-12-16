289a66b0456883036885d11e7ffa2d64326a0d22
Raymond Lo <raymond.lo@yahoo.com>
2022-12-16 15:11:36 -0500

    Add Graves to scene

---
+++ src/World/components/Graves/Graves.ts
@@ -0,0 +1,31 @@
+import { BoxGeometry, Group, Mesh, MeshStandardMaterial } from "three";
+import World from "../../World";
+
+export default class Graves {
+  world: World;
+  constructor() {
+    this.world = World.getInstance();
+
+    const graves = new Group();
+    const cube = new BoxGeometry(0.75, 1, 0.35);
+    const material = new MeshStandardMaterial({ color: "#b2b6b1" });
+
+    for (let i = 0; i < 50; i++) {
+      const angle = Math.random() * Math.PI * 2;
+      const radius = 2 + Math.random() * 8;
+
+      const x = Math.cos(angle) * radius;
+      const z = Math.sin(angle) * radius;
+
+      const grave = new Mesh(cube, material);
+      grave.position.set(x, -0.75, z);
+
+      grave.rotation.z = (Math.random() - 0.5) * 0.4;
+      grave.rotation.y = (Math.random() - 0.5) * 0.4;
+
+      graves.add(grave);
+    }
+
+    this.world.scene.add(graves);
+  }
+}
